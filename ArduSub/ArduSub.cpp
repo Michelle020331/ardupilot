2f803fcfe1d33ad77b2e1f6d3880adb4c5faad1c
Andy Piper <github@andypiper.com>
2024-09-04 10:13:39 +0100

    Sub: Write_Attitude() requires sample time
    log attitude dt

--- ArduSub/ArduSub.cpp
+++ ArduSub/ArduSub.cpp
@@ -136,7 +136,7 @@ void Sub::run_rate_controller()
 {
     const float last_loop_time_s = AP::scheduler().get_last_loop_time_s();
     motors.set_dt(last_loop_time_s);
-    attitude_control.set_dt(last_loop_time_s);
+    attitude_control.set_dt(last_loop_time_s, AP::scheduler().get_loop_start_time_us());
     pos_control.set_dt(last_loop_time_s);
 
     //don't run rate controller in manual or motordetection modes
